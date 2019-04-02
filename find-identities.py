78c95f4a693a44f2818371a1651144367f0511ea
luigibk <41112628+luigibk@users.noreply.github.com>
2019-04-02 14:48:47 +0100

    Update find-identities.py

--- find-identities.py
+++ find-identities.py
@@ -12,11 +12,12 @@ query = json.loads(line)
 def filter_for_env():
     return filter(lambda x: query['env'] in x['environments'], identity_config)
 
+
 with open("identity.yaml", 'r') as stream:
     identity_config = yaml.load(stream, Loader=yaml.FullLoader)
 
-    result = {'identities': ';'.join([x['name'] for x in identity_config if filter_for_env()])}
-    result['keyvault_names'] = ';'.join(["{}-{}".format(x['keyvault']['name'], query['env']) for x in identity_config if filter_for_env()])
-    result['keyvault_rgs'] = ';'.join(["{}-{}".format(x['keyvault']['resource_group'], query['env']) for x in identity_config if filter_for_env()])
+    result = {'identities': ';'.join([x['name'] for x in filter_for_env()])}
+    result['keyvault_names'] = ';'.join(["{}-{}".format(x['keyvault']['name'], query['env']) for x in filter_for_env()])
+    result['keyvault_rgs'] = ';'.join(["{}-{}".format(x['keyvault']['resource_group'], query['env']) for x in filter_for_env()])
 
     print(json.dumps(result))
